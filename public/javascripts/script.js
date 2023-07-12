f794359f87de66cab8df4ced0878b6535c7e0e2b
LauraBGitHub <119501811+LauraBGitHub@users.noreply.github.com>
2023-07-12 11:40:31 +0100

    functionality

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,6 +1,26 @@
-const slider = document.getElementById("Slider01");
-const textInput = document.getElementById("txtInput01");
 
-Slider01.addEventListener("input", function() {
-    txtInput01.value = this.value;
-});
+
+  function updateSliderValue() {
+    var sliderValue = document.getElementById('Slider01').value;
+    document.getElementById('txtInput01').value = sliderValue;
+    var sliderValue = document.getElementById('Slider02').value;
+    document.getElementById('txtInput02').value = sliderValue;
+    var sliderValue = document.getElementById('Slider03').value;
+    document.getElementById('txtInput03').value = sliderValue;
+    var sliderValue = document.getElementById('Slider04').value;
+    document.getElementById('txtInput04').value = sliderValue;
+    var sliderValue = document.getElementById('Slider05').value;
+    document.getElementById('txtInput05').value = sliderValue;
+  }
+  function updateTextInput() {
+    var textInputValue = document.getElementById('txtInput01').value;
+    document.getElementById('Slider01').value = textInputValue;
+    var textInputValue = document.getElementById('txtInput02').value;
+    document.getElementById('Slider02').value = textInputValue;
+    var textInputValue = document.getElementById('txtInput03').value;
+    document.getElementById('Slider03').value = textInputValue;
+    var textInputValue = document.getElementById('txtInput04').value;
+    document.getElementById('Slider04').value = textInputValue;
+    var textInputValue = document.getElementById('txtInput05').value;
+    document.getElementById('Slider05').value = textInputValue;
+  }
