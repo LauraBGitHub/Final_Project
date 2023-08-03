323bff45b4fa82daea4f1c088fafee3a865f3778
LauraBGitHub <119501811+LauraBGitHub@users.noreply.github.com>
2023-08-03 21:36:35 +0100

    Open Zepplin and Trade Contract
    Added open zeplin libararies and Created a V3 Trading contract

--- Trade02.sol
+++ Trade02.sol
@@ -2,31 +2,36 @@
 pragma solidity =0.7.6;
 pragma abicoder v2;
 
-import '@uniswap/v3-periphery/contracts/libraries/TransferHelper.sol';
 import '@uniswap/v3-periphery/contracts/interfaces/ISwapRouter.sol';
-
-contract SwapExamples {
+interface IERC20{ 
+    function approve(address spender, uint256 amount) external returns (bool);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address to, uint256 amount) external returns (bool);
     
-
-    ISwapRouter public immutable swapRouter;
+    
+}
+contract Trade02 {
+    
+    address public constant routerAddress= 0xE592427A0AEce92De3Edee1F18E0157C05861564; 
+    ISwapRouter public immutable swapRouter = ISwapRouter(routerAddress);
 
 
     address public constant WMATIC = 0x9c3C9283D3e44854697Cd22D3Faa240Cfb032889;
     address public constant WETH = 0xA6FA4fB5f76172d178d61B04b0ecd319C5d1C0aa;
 
+    IERC20 public wrapMatic = IERC20(WMATIC); 
+
     // we will set the pool fee to 0.3%.
     uint24 public constant poolFee = 3000;
 
-    constructor(ISwapRouter _swapRouter) {
-        swapRouter = _swapRouter;
-    }
+    constructor() { }
 
     function swapExactInputSingle(uint256 amountIn) external returns (uint256 amountOut) {
        
-        //TransferHelper.safeTransferFrom(WMATIC, msg.sender, address(this), amountIn);
+        //wrapMatic.safeTransferFrom(WMATIC, msg.sender, address(this), amountIn);
 
         // Approve the router to spend WMATIC.
-        TransferHelper.safeApprove(WMATIC, address(swapRouter), amountIn);
+        wrapMatic.approve(address(swapRouter), amountIn);
 
         // Naively set amountOutMinimum to 0. In production, use an oracle or other data source to choose a safer value for amountOutMinimum.
         // We also set the sqrtPriceLimitx96 to be 0 to ensure we swap our exact input amount.
@@ -48,12 +53,9 @@ contract SwapExamples {
 
     
     function swapExactOutputSingle(uint256 amountOut, uint256 amountInMaximum) external returns (uint256 amountIn) {
-        // Transfer the specified amount of WMATIC to this contract.
-        TransferHelper.safeTransferFrom(WMATIC, msg.sender, address(this), amountInMaximum);
-
         // Approve the router to spend the specifed `amountInMaximum` of WMATIC.
         
-        TransferHelper.safeApprove(WMATIC, address(swapRouter), amountInMaximum);
+        wrapMatic.approve( address(swapRouter), amountInMaximum);
 
         ISwapRouter.ExactOutputSingleParams memory params =
             ISwapRouter.ExactOutputSingleParams({
@@ -73,8 +75,8 @@ contract SwapExamples {
         // For exact output swaps, the amountInMaximum may not have all been spent.
         // If the actual amount spent (amountIn) is less than the specified maximum amount, we must refund the msg.sender and approve the swapRouter to spend 0.
         if (amountIn < amountInMaximum) {
-            TransferHelper.safeApprove(WMATIC, address(swapRouter), 0);
-            TransferHelper.safeTransfer(WMATIC, address(this), amountInMaximum - amountIn);
+            wrapMatic.approve(address(swapRouter), 0);
+            wrapMatic.Transfer(address(this), amountInMaximum - amountIn);
         }
     }
 }
